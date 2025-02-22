2409b1bced1f910ae0fb6bfe04b4e37a16abc3d4
ChelmiLave <velaangel986@gmail.com>
2025-02-21 21:13:06 -0700

    Initial commit

---
+++ src/components/heading.js
@@ -0,0 +1,5 @@
+import React from "react";
+function Heading() {
+  return <h1>My favorite food is</h1>;
+}
+export default Heading;
---
+++ src/components/list.js
@@ -0,0 +1,13 @@
+import React from "react";
+function List() {
+  return (
+    <div>
+      <ul>
+        <li>Beacon</li>
+        <li>Sushi</li>
+        <li>Enchiladas</li>
+      </ul>
+    </div>
+  );
+}
+export default List;
